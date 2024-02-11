c6b5ba3675c57376cc834a46ea7949e7967b6371
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-02-12 00:09:45 +0530

    Update Products.js

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,39 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const products = [
+  {
+    id:1,
+    title: "Test1",
+    price: 6,
+    quantity: 1,
+    description: "This is a first product - amazing!",
+  },
+  {
+    id:2,
+    title: "Test2",
+    price: 9,
+    quantity: 1,
+    description: "This is a second product - amazing!",
+  },
+  {
+    id:3,
+    title: "Test3",
+    price: 3,
+    quantity: 1,
+    description: "This is a third product - amazing!",
+  },
+];
 
 const Products = (props) => {
+  
+  const items=products.map((item)=> <ProductItem key={item.id} id={item.id} title={item.title} quantity={item.quantity} price={item.price} description={item.description}/>
+  )
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {items}
       </ul>
     </section>
   );
