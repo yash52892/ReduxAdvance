4e26fc29395209402467743d999bcfd87ea6b925
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-02-12 00:07:49 +0530

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,22 @@
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
+import { useSelector } from 'react-redux';
 
 const Cart = (props) => {
-
+  const items=useSelector(state=>state.cart.cartItems)
+  const total=useSelector(state=>state.cart.total);
+ const cartdispayitems=items.map((i)=> <CartItem item={{id:i.id, title: i.title, quantity: i.quantity, price: i.price }}
+/>)
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {cartdispayitems}
       </ul>
+      <span>
+      <h2>Total</h2>${total.toFixed(2)}
+      </span>
     </Card>
   );
 };
