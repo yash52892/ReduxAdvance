fc9c1a89c64cefbad93de8c4cabfc133771a08a1
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-02-12 00:08:04 +0530

    Update CartButton.js

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,9 @@
 import { uiAction } from '../../Store/uiSlice';
 import classes from './CartButton.module.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 const CartButton = (props) => {
   const dispatch=useDispatch();
+  const noi=useSelector(state=>state.cart.numOfItem);
 const handlecartvisibile = () =>{
  dispatch(uiAction.toggle());
 }
@@ -10,7 +11,7 @@ const handlecartvisibile = () =>{
   return (
     <button onClick={handlecartvisibile} className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{noi}</span>
     </button>
   );
 };
