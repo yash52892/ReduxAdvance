721be90c23db52d98a45e02c11ce40b7e3b13162
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-02-12 23:23:14 +0530

    Update uiSlice.js

--- src/Store/uiSlice.js
+++ src/Store/uiSlice.js
@@ -2,11 +2,18 @@ import { createSlice} from "@reduxjs/toolkit";
 
 const uicartSlice = createSlice({
     name:"UI",
-    initialState: {cartVisible: false},
+    initialState: {cartVisible: false, notification:null},
     reducers:{
         toggle(state) {
             state.cartVisible = !state.cartVisible;
-        }
+        },
+        showNotification(state, action) {
+            state.notification = {
+              status: action.payload.status,
+              title: action.payload.title,
+              message: action.payload.message,
+            };
+        },
     }
 })
 
