d373038c71907137ca5bfebf22f8ae2c6a323e50
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-02-14 17:49:10 +0530

    Update cartSlice.js

--- src/Store/cartSlice.js
+++ src/Store/cartSlice.js
@@ -1,11 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initCartState={cartItems:[], total:0, numOfItem:0}
+const initCartState={cartItems:[], total:0, numOfItem:0, changed:false}
 
 const cartSlice = createSlice({
     name: "cart",
     initialState:initCartState,
     reducers:{
+        replaceCart(state, action){
+            state.cartItems=action.payload.cartItems;
+            state.numOfItem=action.payload.numOfItem;
+            state.total=action.payload.total;
+        },
         increment(state,action){
             const index = state.cartItems.findIndex(x => x.id ===action.payload.id);
             
@@ -13,6 +18,7 @@ const cartSlice = createSlice({
                 state.cartItems[index].quantity++;
                 state.numOfItem++;
                 state.total=state.total+action.payload.price;
+                state.changed=true;
             }
         },
         decrement(state, action){
@@ -21,6 +27,7 @@ const cartSlice = createSlice({
                 if(state.cartItems[index].quantity>1)
                 {
                 state.cartItems[index].quantity--;
+                state.changed=true;
                 state.total=state.total-action.payload.price
                 state.numOfItem--;
                 }
@@ -29,6 +36,7 @@ const cartSlice = createSlice({
                     state.cartItems.splice(index, 1);
                     state.total=state.total-action.payload.price
                     state.numOfItem--;
+                    state.changed=true;
                 }
             }
         },
@@ -39,15 +47,17 @@ const cartSlice = createSlice({
                 state.cartItems[index].quantity++;
                 state.numOfItem++; 
                 state.total=state.total+action.payload.price
+                state.changed=true;
             }
             else{
                 state.cartItems.push(action.payload);
                 state.numOfItem++;
                 state.total=state.total+action.payload.price;
+                state.changed=true;
             }
         },
-    }
-})
+    },
+});
 
 export default cartSlice;
 export const cartActions= cartSlice.actions;
